c077bf47fb030134cb9ad7a3ae8f33d2d38ce29f
Dogus <dogus@next-game.com>
2018-02-19 19:09:43 +0000

    Created Projects

---
+++ Pluto/Pluto.UnitTests/Rover.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pluto.UnitTests
+{
+    [TestClass]
+    public class Rover
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
