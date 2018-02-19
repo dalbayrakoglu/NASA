bf8cab12be6402e3253db5648c8b34b1b76de059
Dogus <dogus@next-game.com>
2018-02-19 19:31:23 +0000

    fails back command

--- Pluto/Pluto.UnitTests/RoverTests.cs
+++ Pluto/Pluto.UnitTests/RoverTests.cs
@@ -16,5 +16,16 @@ namespace Pluto.UnitTests
             Assert.AreEqual(rover.GetX(), 0);
             Assert.AreEqual(rover.GetY(), 1);
         }
+
+        [TestMethod]
+        public void ExecuteOrder_Backward_0_1()
+        {
+            Rover rover = new Rover(5, 5, 'N');
+
+            rover.ExecuteOrder("B");
+
+            Assert.AreEqual(rover.GetX(), 5);
+            Assert.AreEqual(rover.GetY(), 4);
+        }
     }
 }
